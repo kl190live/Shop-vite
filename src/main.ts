6834a504d659b670adffe03f4c2c841c4b165c79
Petrik Diak <diak@petrik.hu>
2023-10-06 12:01:37 +0200

    kesz

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,37 @@
 import { termek } from "./Termek";
 
+let List : termek[]=[];
+
+
+function Kiiras()
+{
+  let table=document.getElementById('table')
+  table!.innerHTML='';
+  for(const t of List)
+  {
+    let tr=document.createElement('tr');
+    tr.textContent=`${t.name} ${t.price} ${t.product}`
+
+    table!.appendChild(tr);
+  }
+}
+
 function newProduct()
 {
   let Name=(document.getElementById('name')as HTMLInputElement).value;
-  let Price=(document.getElementById('price')as HTMLInputElement).value;
+  let Price=parseInt((document.getElementById('price')as HTMLInputElement).value);
   let ProductCode=(document.getElementById('productCode')as HTMLInputElement).value;
-  
 
+  let Termek=new termek(Name,Price,ProductCode)
+  List.push(Termek);
 
-
-
+  Kiiras();
 
 }
 
 function init()
 {
-  document.getElementById('button')?.addEventListener('submit',newProduct)
+  document.getElementById('button')!.addEventListener('submit',newProduct)
 }
 
 document.addEventListener('DOMContentLoaded', init)
